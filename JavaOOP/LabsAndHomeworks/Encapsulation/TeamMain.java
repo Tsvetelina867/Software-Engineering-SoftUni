ce9a5047ddc583744d55f53c1c69cb8e7b3bc86d
Tsvetelina867 <cvettyasd@gmail.com>
2023-10-28 11:31:52 +0300

    Added Encapsulation lab solutions

---
+++ JavaOOP/LabsAndHomeworks/Encapsulation/TeamMain.java
@@ -0,0 +1,38 @@
+package Encapsulation;
+
+import Encapsulation.Person;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Optional;
+import java.util.Scanner;
+
+public class TeamMain {
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(reader.readLine());
+
+        Team team = new Team("Black Eagles");
+
+        for (int i = 0; i < n; i++) {
+            String[] input = reader.readLine().split(" ");
+
+            Optional<Person> person = Optional.empty();
+
+            try {
+                person = Optional.of(new Person(input[0], input[1], Integer.parseInt(input[2]), Double.parseDouble(input[3])));
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+
+            person.ifPresent(team::addPlayer);
+        }
+
+        System.out.println("First team have " + team.getFirstTeam().size() + " players");
+        System.out.println("Reserve team have " + team.getReserveTeam().size() + " players");
+
+    }
+}
